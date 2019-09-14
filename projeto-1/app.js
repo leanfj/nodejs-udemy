d2541306c87c47f94b5fa0155091928051d36d91
leanfj <leanfj@hotmail.com>
2019-09-14 19:17:11 -0300

    Removendo body-parser

--- projeto-1/app.js
+++ projeto-1/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 
+// const bodyParser = require("body-parser");
+
 const app = express();
+
+const users = [];
 
 app.set("view engine", "pug");
 
 app.set("views", "views");
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res, next) => {
   res.render("index", { pageTitle: "Add Users" });
@@ -15,6 +21,7 @@ app.get("/users", (req, res, next) => {
 });
 
 app.post("/add-user", (req, res, next) => {
+  users.push({ name: req.body.userName });
   res.redirect("/users");
 });
 
