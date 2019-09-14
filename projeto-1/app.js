53d236e1194645ad10b05ba5997c655ba00bf177
leanfj <leanfj@hotmail.com>
2019-09-14 19:24:37 -0300

    Mundando view engine para handlebars

--- projeto-1/app.js
+++ projeto-1/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
+const handlebars = require("express-handlebars");
+
 const app = express();
 
 const users = [];
 
-app.set("view engine", "pug");
-
+app.set("hbs", handlebars());
+app.set("view engine", "hbs");
 app.set("views", "views");
 
 app.use(express.urlencoded({ extended: false }));
