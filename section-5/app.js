3bf6af04b23ca9056b687fd8abc3fb4875ca7590
leanfj <leanfj@hotmail.com>
2019-09-16 00:23:33 -0300

    Indentificando conceito de middleware

---
+++ section-5/app.js
@@ -0,0 +1,20 @@
+const http = require("http");
+
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("In new middleware");
+
+  res.json({ message: "Ola" });
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
