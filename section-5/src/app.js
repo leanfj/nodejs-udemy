fbbeda5d7baaea6f7684974a7944ef1901afdf5e
leanfj <leanfj@hotmail.com>
2019-09-17 08:00:12 -0300

    Controlando fluxo com rotas

--- section-5/src/app.js
+++ section-5/src/app.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next();
+app.use("/add-product", (req, res, next) => {
+  console.log("Add Product route");
+  res.send("<h1>Add Product route</h1>");
 });
 
-app.use((req, res) => {
+app.use("/", (req, res) => {
   console.log("In new middleware");
 
   res.send("<h1>Hellow from express</h1>");
