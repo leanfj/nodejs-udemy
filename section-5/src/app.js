873329999ff5ad1bdded0aa036fe7f6c1236bcd8
leanfj <leanfj@hotmail.com>
2019-09-17 23:37:39 -0300

    Corrigindo nome de rotas e adicionando pagina 404

--- section-5/src/app.js
+++ section-5/src/app.js
@@ -10,4 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(adminRoutes);
 app.use(homeRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Pagina não encontrada</h1>');
+});
+
 app.listen(3000);
--- section-5/src/routes/admin.js
+++ section-5/src/routes/admin.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/adicionar-aparelho', (req, res, next) => {
+router.get('/aparelho', (req, res, next) => {
   res.send(
-    '<form action="/aparelho" method="POST"><input type="text" name="modelo"><button type="submit">Enviar</button></input></form>'
+    '<form action="/adicionar-aparelho" method="POST"><input type="text" name="modelo"><button type="submit">Enviar</button></input></form>'
   );
 });
 
-router.post('/aparelho', (req, res, next) => {
+router.post('/adicionar-aparelho', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
