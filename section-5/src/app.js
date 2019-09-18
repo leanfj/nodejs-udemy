35e18ef109d4b8c085925da97efd8df3a5607998
leanfj <leanfj@hotmail.com>
2019-09-17 23:06:04 -0300

    Enviar dados via form

--- section-5/src/app.js
+++ section-5/src/app.js
@@ -1,16 +1,22 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.use("/add-product", (req, res, next) => {
-  console.log("Add Product route");
-  res.send("<h1>Add Product route</h1>");
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/adicionar-aparelho', (req, res, next) => {
+  res.send(
+    '<form action="/aparelho" method="POST"><input type="text" name="modelo"><button type="submit">Enviar</button></input></form>'
+  );
 });
 
-app.use("/", (req, res) => {
-  console.log("In new middleware");
+app.use('/aparelho', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
 
-  res.send("<h1>Hellow from express</h1>");
+app.use('/', (req, res, next) => {
+  res.send('<h1>GAMM-APP</h1>');
 });
 
 app.listen(3000);
