fa97f5dc2ce9ab6ae7ec7c77e8a51f6a3eee99b7
Leandro Ferreira <leanfj@hotmail.com>
2021-12-24 10:09:34 -0300

    Trabalhando com rotas

--- express-section/app.js
+++ express-section/app.js
@@ -2,12 +2,12 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res, next) => {
-    console.log("In the first middleware")
-    next()
+app.use('/add-product', (req, res, next) => {
+    console.log('Add-product')
+    res.send('<h1>Add product page</h1>')
 })
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log("In the second middleware")
     res.send('<h1> Hello from express<h1>')
 })
